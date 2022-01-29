9b8b5128cda54bf6c2fe7e43b7c482b1395eb955
Adriviciano <97399669+Adriviciano@users.noreply.github.com>
2022-01-29 10:50:40 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-//de momento vacia aun xD
+let cas5 = document.getElementById("casilla5")
+let chat = document.getElementById("mensaje")
+
+if (cas5.onclick===true){
+   cas5.style.backgroundImage = url("fichaX.png")
+   chat.textContent = "Turno del jugador 2"
+}
